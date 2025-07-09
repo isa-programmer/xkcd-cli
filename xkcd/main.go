ca28cda53501782db50e2b4631892836b61c424e
Isa <me.isa4@protonmail.com>
2025-07-09 15:47:09 +0300

    Commit

---
+++ xkcd/main.go
@@ -0,0 +1,106 @@
+package main
+
+// LICENSE: GPLv3
+// Author: İsa
+// URL: https://github.com/isa-programmer/xkcd-cli
+// Description: A command-line interface for xkcd.com comics
+// Usage:
+//		xkcd-cli # A random comic
+// 		xkcd-cli 927 # You can choose comic ID yourself
+
+import (
+	"net/http"
+	"os/exec"
+	"math/rand"
+	"github.com/charmbracelet/lipgloss"
+	"time"
+	"strconv"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func printXkcdComic(comic XkcdJsonStruct) {
+	var output string
+	output = fmt.Sprintf(`
+	- Comic Link: https://xkcd.com/%d
+	- Title: %s
+	- Date: %s/%s/%s
+	Description: %s
+	`,comic.Num,comic.Title,comic.Day,comic.Month,comic.Year,comic.Alt)
+	
+	style := lipgloss.NewStyle().
+				Width(80).
+				Border(lipgloss.ThickBorder()).
+				BorderForeground(lipgloss.Color("#820173")).
+				Background(lipgloss.Color("#56034c"))
+
+	fmt.Println(style.Render(output))
+}
+
+type XkcdJsonStruct struct{
+	Year 	string	`json:"year"`
+	Month 	string	`json:"month"`
+	Day 	string	`json:"day"`
+	Title 	string	`json:"title"`
+	Alt 	string	`json:"alt"`
+	Img 	string	`json:"img"`
+	Num 	int		`json:"num"`
+}
+
+func fetchComic(comicId int) (XkcdJsonStruct, error){
+	var comic XkcdJsonStruct
+	
+	url := fmt.Sprintf("https://xkcd.com/%d/info.0.json",comicId)
+	
+	resp, err := http.Get(url)
+	if err != nil{
+		return comic,err
+	}
+
+	if resp.StatusCode != 200 {
+		return comic, fmt.Errorf("failed to fetch comic %d: %s", comicId, resp.Status)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&comic)
+	if err != nil{
+		return comic,err
+	}
+
+	return comic,nil
+}
+
+func generateRandomComicNumber() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(3112) + 1
+}
+
+func main(){
+	var comicId int
+	var err error
+	if len(os.Args) > 1 {
+		comicId, err = strconv.Atoi(os.Args[1])
+		if err != nil{
+			fmt.Printf("Comic ID should be an integer! Not %s",os.Args[1])
+			return
+		}
+	} else {
+		comicId = generateRandomComicNumber()
+	}
+	
+	comic,err := fetchComic(comicId)
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	
+	printXkcdComic(comic)
+
+	cmd := exec.Command("feh",comic.Img)
+
+	err = cmd.Run()
+	if err != nil{
+		fmt.Println(err)
+	}
+	
+}
